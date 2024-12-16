04584e48a9dbbf1ebf963ee03da73304bc6b27b0
princesslucu <salsabilakeisha041@gmail.com>
2024-12-16 09:41:17 +0700

    dibuat oleh keisha

---
+++ views/v_contoh.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Gelo<h1>
+    <p>ini dibuat oleh keisha</p>
+</body>
+</html>
